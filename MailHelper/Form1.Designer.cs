b737c1f955d128c9282b614bd8937f89dfdb3283
FlyFeatherFlight <saihua_chen@outllok.com>
2019-04-15 17:17:36 +0800

    邮件测试

--- MailHelper/Form1.Designer.cs
+++ MailHelper/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(478, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(206, 60);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(249, 21);
+            this.emailBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "邮箱地址：";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(206, 158);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(249, 213);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // titleBox
+            // 
+            this.titleBox.Location = new System.Drawing.Point(206, 131);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(249, 21);
+            this.titleBox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "标题：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "正文：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(478, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.titleBox);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- MailHelper/Form1.cs
+++ MailHelper/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace MailHelper
@@ -15,6 +9,37 @@ namespace MailHelper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MailHelper mailHelper = new MailHelper();
+            //mailHelper.Host = "smtp.exmail.qq.com";
+            //mailHelper.MailFrom ="system-message@chiccasa.com.cn";
+            //mailHelper.MailPwd ="Chic123456789";
+
+            mailHelper.Host = "smtp.exmail.qq.com";
+            mailHelper.MailFrom = "11@chiccasa.com.cn";
+            mailHelper.MailPwd = "Chic123456";
+            //mailHelper.MailFrom = "software-engineer@chiccasa.com.cn";
+            //mailHelper.MailPwd = "Hua19931020";
+
+            //mailHelper.Host = "smtp-mail.outlook.com";
+            //mailHelper.MailFrom = "saihua_chen@outlook.com";
+            //mailHelper.MailPwd = "c1101269325";
+            mailHelper.MailSubject = titleBox.Text;
+            mailHelper.MailBody = richTextBox1.Text;
+            mailHelper.MailToArray = new string[1] { emailBox.Text };
+            try
+            {
+                mailHelper.Send();
+                label4.Text = "发送成功！";
+            }
+            catch (Exception ex)
+            {
+                label4.Text = "发送失败！" + ex.Message;
+                throw ex;
+            }
         }
     }
 }
